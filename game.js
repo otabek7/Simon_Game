e14e0779e05ccab006a58e172674fbbccea6b072
Otabek <mavlonovo@tamu.edu>
2023-07-28 23:31:44 -0500

    added fade out effect for button click

--- game.js
+++ game.js
@@ -8,7 +8,7 @@ $(".btn").click(function () {
   var userChosenColour = $(this).attr("id");
   userClickedPattern.push(userChosenColour);
   playSound(userChosenColour);
-  console.log(userClickedPattern);
+  animatePress(userChosenColour);
 });
 
 function nextSequence() {
@@ -31,4 +31,11 @@ function nextSequence() {
 function playSound(name) {
   var audio = new Audio("sounds/" + name + ".mp3");
   audio.play();
+}
+
+function animatePress(currentColour) {
+  $("#" + currentColour).addClass("pressed");
+  setTimeout(function () {
+    $("#" + currentColour).removeClass("pressed");
+  }, 100);
 }
