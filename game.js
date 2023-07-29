a9d56894d38be436baa8aae786cb469a712b82da
Otabek <mavlonovo@tamu.edu>
2023-07-28 23:16:47 -0500

    added functions to js

---
+++ game.js
@@ -0,0 +1,27 @@
+var buttonColours = ["red", "blue", "green", "yellow"];
+
+var userClickedPattern = [];
+
+var gamePattern = [];
+
+$(".btn").click(function () {
+  var userChosenColour = $(this).attr("id");
+  userClickedPattern.push(userChosenColour);
+});
+
+function nextSequence() {
+  var randomNumber = Math.floor(Math.random() * (3 - 0 + 1) + 0);
+
+  var randomChosenColour = buttonColours[nextSequence()];
+
+  gamePattern.push(randomChosenColour);
+
+  $("#" + randomChosenColour)
+    .fadeOut(100)
+    .fadeIn(100)
+    .fadeOut(100)
+    .fadeIn(100);
+
+  var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+  audio.play();
+}
