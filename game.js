a80b384581f7b4e8d6b1de521a358598e670ba63
Otabek <mavlonovo@tamu.edu>
2023-07-29 14:03:15 -0500

    added startOver function

--- game.js
+++ game.js
@@ -79,5 +79,13 @@ function checkAnswer(currentLevel) {
     }, 200);
 
     $("#level-title").text("Game Over, Press Any Key to Restart");
+    startOver();
   }
+}
+
+function startOver(){
+  level = 0;
+  gamePattern = [];
+  gameStarted = false;
+
 }
